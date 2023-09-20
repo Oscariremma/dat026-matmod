d1f0bc066d63c94b6f65f31abd25a5f5086206fa
Oscar Eriksson <oscariremma@gmail.com>
2023-09-20 21:18:31 +0200

    Move apart if inside balls or outside walls

--- module4/src/inputs.rs
+++ module4/src/inputs.rs
@@ -1,10 +1,25 @@
 use crate::components::*;
+use crate::physics::is_approx_colliding;
 use crate::BALLS_Z_INDEX;
 use bevy::utils::Instant;
 use bevy::window::{PrimaryWindow, Window};
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use rand::Rng;
 use std::ops::Mul;
+
+macro_rules! debounce_return {
+    () => {
+        static mut LAST_CLICK: Option<Instant> = None;
+        unsafe {
+            if let Some(last_click) = LAST_CLICK {
+                if last_click.elapsed().as_secs_f32() < 0.1 {
+                    return;
+                }
+            }
+            LAST_CLICK = Some(Instant::now());
+        }
+    };
+}
 
 pub fn get_real_world_pos(
     q_windows: Query<&Window, With<PrimaryWindow>>,
@@ -30,28 +45,32 @@ pub fn handle_left_click(
     mut materials: ResMut<Assets<ColorMaterial>>,
     q_windows: Query<&Window, With<PrimaryWindow>>,
     camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    balls_query: Query<(&Transform, With<Ball>)>,
 ) {
-    // Very hacky solution to prevent double spawning
-    static mut LAST_CLICK: Option<Instant> = None;
-    unsafe {
-        if let Some(last_click) = LAST_CLICK {
-            if last_click.elapsed().as_secs_f32() < 0.1 {
-                return;
-            }
-        }
-        LAST_CLICK = Some(Instant::now());
-    }
+    debounce_return!();
+
+    let pos = get_real_world_pos(q_windows, camera_q);
 
     let mut rng = rand::thread_rng();
+    let size = rng.gen_range(50.0..200.0);
+
+    for (transform, _) in balls_query.iter() {
+        if is_approx_colliding(
+            transform.translation.truncate(),
+            transform.scale.x,
+            pos,
+            size,
+        ) {
+            return;
+        }
+    }
 
     let color = Color::hsl(rng.gen_range(0.0..=360.0), 1.0, 0.5)
         .as_rgba()
         .mul(3.0);
 
-    let size = rng.gen_range(50.0..200.0);
     let mass = size / 10.0;
 
-    let pos = get_real_world_pos(q_windows, camera_q);
     commands.spawn((
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::default().into()).into(),
@@ -69,17 +88,9 @@ pub fn handle_right_click(
     mut commands: Commands,
     q_windows: Query<&Window, With<PrimaryWindow>>,
     camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
-    mut balls_query: Query<(Entity, &Transform, With<Ball>)>,
+    balls_query: Query<(Entity, &Transform, With<Ball>)>,
 ) {
-    static mut LAST_CLICK: Option<Instant> = None;
-    unsafe {
-        if let Some(last_click) = LAST_CLICK {
-            if last_click.elapsed().as_secs_f32() < 0.1 {
-                return;
-            }
-        }
-        LAST_CLICK = Some(Instant::now());
-    }
+    debounce_return!();
     let pos = get_real_world_pos(q_windows, camera_q);
 
     for (entity, transform, _) in balls_query.iter() {
--- module4/src/physics.rs
+++ module4/src/physics.rs
@@ -5,7 +5,7 @@ use bevy::window::PrimaryWindow;
 
 pub fn handle_for_edge_collisions(
     window_query: Query<&Window, With<PrimaryWindow>>,
-    mut balls_query: Query<(&Transform, &mut Velocity, &Ball)>,
+    mut balls_query: Query<(&mut Transform, &mut Velocity, &Ball)>,
     time_step: Res<FixedTime>,
 ) {
     let window = window_query.get_single().expect("Window should exist.");
@@ -15,7 +15,7 @@ pub fn handle_for_edge_collisions(
     let window_top = window.height() / 2.0;
     let window_bottom = -window.height() / 2.0;
 
-    for (transform, mut velocity, _) in &mut balls_query {
+    for (mut transform, mut velocity, _) in &mut balls_query {
         let new_edge_x = next_frame_edge(
             transform.translation.x,
             velocity.x,
@@ -30,20 +30,32 @@ pub fn handle_for_edge_collisions(
             &time_step,
         );
 
-        if new_edge_x > window_right || new_edge_x < window_left {
+        if new_edge_x > window_right && velocity.x > 0.0
+            || new_edge_x < window_left && velocity.x < 0.0
+        {
             velocity.x *= -1.0;
         }
 
-        if new_edge_y > window_top || new_edge_y < window_bottom {
+        if new_edge_y > window_top && velocity.y > 0.0
+            || new_edge_y < window_bottom && velocity.y < 0.0
+        {
             velocity.y *= -1.0;
+        }
+
+        let current_edge_x = calculate_edge(transform.translation.x, transform.scale.x);
+        let current_edge_y = calculate_edge(transform.translation.y, transform.scale.y);
+
+        if current_edge_x > window_right || current_edge_x < window_left {
+            transform.translation.x -= transform.translation.x.signum();
+        }
+
+        if current_edge_y > window_top || current_edge_y < window_bottom {
+            transform.translation.y -= transform.translation.y.signum();
         }
     }
 }
 
-pub fn apply_velocity(
-    mut query: Query<(&mut Transform, &Velocity)>,
-    time_step: Res<FixedTime>,
-) {
+pub fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<FixedTime>) {
     for (mut transform, velocity) in &mut query {
         transform.translation.x += velocity.x * time_step.period.as_secs_f32();
         transform.translation.y += velocity.y * time_step.period.as_secs_f32();
@@ -61,19 +73,53 @@ pub fn apply_gravity(
 }
 
 pub fn handle_inter_ball_collision(
-    mut balls_query: Query<(&Transform, &mut Velocity, &Ball)>,
+    mut balls_query: Query<(&mut Transform, &mut Velocity, &Ball)>,
     time_step: Res<FixedTime>,
 ) {
     let mut ball_combinations = balls_query.iter_combinations_mut();
 
     while let Some([mut ball_a, mut ball_b]) = ball_combinations.fetch_next() {
-        if is_colliding(&time_step, &ball_a, &ball_b) {
+        let (ref mut transform_a, velocity_a, _) = ball_a;
+        let (ref mut transform_b, velocity_b, _) = ball_b;
+
+        if is_colliding(
+            transform_a.translation.truncate(),
+            transform_a.scale.x,
+            transform_b.translation.truncate(),
+            transform_b.scale.x,
+        ) {
+            move_apart(&mut ball_a.0, &mut ball_b.0);
+            continue;
+        }
+
+        let mut ball_a = (transform_a.as_ref(), velocity_a, ball_a.2);
+        let mut ball_b = (transform_b.as_ref(), velocity_b, ball_b.2);
+
+        if will_collide(&time_step, &mut ball_a, &mut ball_b) {
             bounce(&mut ball_a, &mut ball_b);
         }
     }
 }
 
-fn is_colliding(
+pub fn is_approx_colliding(
+    center_pos_a: Vec2,
+    width_a: f32,
+    center_pos_b: Vec2,
+    width_b: f32,
+) -> bool {
+    let width_a = width_a / 2.0;
+    let width_b = width_b / 2.0;
+
+    let a_min = center_pos_a - width_a;
+    let a_max = center_pos_a + width_a;
+
+    let b_min = center_pos_b - width_b;
+    let b_max = center_pos_b + width_b;
+
+    a_min.x < b_max.x && a_max.x > b_min.x && a_min.y < b_max.y && a_max.y > b_min.y
+}
+
+fn will_collide(
     time_step: &Res<FixedTime>,
     ball_a: &(&Transform, Mut<Velocity>, &Ball),
     ball_b: &(&Transform, Mut<Velocity>, &Ball),
@@ -93,10 +139,23 @@ fn is_colliding(
         time_step,
     );
 
-    let next_distance = next_position_a.distance(next_position_b);
+    is_colliding(
+        next_position_a,
+        transform_a.scale.x,
+        next_position_b,
+        transform_b.scale.x,
+    )
+}
 
-    let combined_size = (transform_a.scale.x + transform_b.scale.x) / 2.0;
-    let next_edge_distance = next_distance - combined_size;
+fn is_colliding(pos_a: Vec2, width_a: f32, pos_b: Vec2, width_b: f32) -> bool {
+    if !is_approx_colliding(pos_a, width_a, pos_b, width_b) {
+        return false;
+    }
+
+    let distance = pos_a.distance(pos_b);
+
+    let combined_size = (width_a + width_b) / 2.0;
+    let next_edge_distance = distance - combined_size;
 
     next_edge_distance <= 0.0
 }
@@ -124,6 +183,25 @@ fn bounce(
 
     velocity_a.0 += new_speed_a_project;
     velocity_b.0 += new_speed_b_project;
+}
+
+fn move_apart(transform_a: &mut Transform, transform_b: &mut Transform) {
+    let vec_between = (transform_b.translation.truncate() - transform_a.translation.truncate())
+        .normalize()
+        .extend(0.0);
+
+    let distance = transform_a
+        .translation
+        .truncate()
+        .distance(transform_b.translation.truncate());
+
+    let combined_size = (transform_a.scale.x + transform_b.scale.x) / 2.0;
+    let next_edge_distance = distance - combined_size;
+
+    let move_amount = next_edge_distance / 2.0;
+
+    transform_a.translation += vec_between * move_amount;
+    transform_b.translation -= vec_between * move_amount;
 }
 
 fn next_frame_edge(center: f32, velocity: f32, diameter: f32, time_step: &Res<FixedTime>) -> f32 {
