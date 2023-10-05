72e7385a006d562bd24370689f7700b1886a6110
Oscar Eriksson <oscariremma@gmail.com>
2023-10-05 23:02:18 +0200

    Touch input added but not tested
    I don't have a touch screen so I can not test and debug it.

--- module4/src/inputs.rs
+++ module4/src/inputs.rs
@@ -1,6 +1,8 @@
 use crate::components::*;
 use crate::physics::is_approx_colliding;
 use crate::BALLS_Z_INDEX;
+use bevy::input::touch::Touch;
+use bevy::input::touch::*;
 use bevy::utils::Instant;
 use bevy::window::{PrimaryWindow, Window};
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
@@ -22,7 +24,7 @@ macro_rules! debounce_return {
     };
 }
 
-pub fn get_real_world_pos(
+fn get_real_world_pos_from_cursor(
     q_windows: Query<&Window, With<PrimaryWindow>>,
     camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
 ) -> Vec2 {
@@ -40,6 +42,39 @@ pub fn get_real_world_pos(
     return Vec2::new(0.0, 0.0);
 }
 
+fn get_real_world_pos_from_touch(
+    camera_q: &Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    touch: &Touch,
+) -> Vec2 {
+    let (camera, camera_transform) = camera_q.single();
+
+    camera
+        .viewport_to_world(camera_transform, touch.position())
+        .map(|ray| ray.origin.truncate())
+        .unwrap_or(Vec2::new(0.0, 0.0))
+}
+
+pub fn handle_touch(
+    touches: Res<Touches>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    balls_query: Query<(&Transform, With<Ball>)>,
+) {
+    for touch in touches.iter_just_pressed() {
+        let pos = get_real_world_pos_from_touch(&camera_q, &touch);
+
+        try_spawn_ball_at(
+            pos,
+            &mut meshes,
+            &mut materials,
+            &balls_query,
+            &mut commands,
+        );
+    }
+}
+
 pub fn handle_left_click(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -50,8 +85,24 @@ pub fn handle_left_click(
 ) {
     debounce_return!();
 
-    let pos = get_real_world_pos(q_windows, camera_q);
+    let pos = get_real_world_pos_from_cursor(q_windows, camera_q);
 
+    try_spawn_ball_at(
+        pos,
+        &mut meshes,
+        &mut materials,
+        &balls_query,
+        &mut commands,
+    );
+}
+
+fn try_spawn_ball_at(
+    pos: Vec2,
+    mut meshes: &mut ResMut<Assets<Mesh>>,
+    mut materials: &mut ResMut<Assets<ColorMaterial>>,
+    balls_query: &Query<(&Transform, With<Ball>)>,
+    commands: &mut Commands,
+) -> bool {
     let mut rng = rand::thread_rng();
     let size = rng.gen_range(50.0..200.0);
 
@@ -62,7 +113,7 @@ pub fn handle_left_click(
             pos,
             size,
         ) {
-            return;
+            return false;
         }
     }
 
@@ -83,6 +134,7 @@ pub fn handle_left_click(
         Ball { mass },
         Velocity(Vec2::new(0.0, 0.0)),
     ));
+    true
 }
 
 pub fn handle_right_click(
@@ -92,7 +144,7 @@ pub fn handle_right_click(
     balls_query: Query<(Entity, &Transform, With<Ball>)>,
 ) {
     debounce_return!();
-    let pos = get_real_world_pos(q_windows, camera_q);
+    let pos = get_real_world_pos_from_cursor(q_windows, camera_q);
 
     for (entity, transform, _) in balls_query.iter() {
         if transform.translation.truncate().distance(pos) < transform.scale.x / 2.0 {
